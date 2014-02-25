c1339226d2ed5462af79c5033c2c4b1922a14674
Barry Niemuth <bn3737@yahoo.com>
2014-02-25 15:15:32 -0600

    Polished

--- todo-list.php
+++ todo-list.php
@@ -9,55 +9,84 @@
 <?php
 
 $filename = "data/todo_list.txt";
-$handle = fopen($filename, "r");
-$contents = fread($handle, filesize($filename));
-if(filesize ($filename) > 0);
 
-//Explode with \n then fclose
-$contents_array = explode("\n", $contents);
-fclose($handle);
+function openfile($filename){
+
+	if(filesize ($filename) > 0) {
+		$handle = fopen($filename, "r");
+		$contents = fread($handle, filesize($filename));
+
+		//Explode with \n then fclose
+		fclose($handle);
+
+		return explode("\n", $contents);
+	}
+	else
+	{
+		return array();
+	}
+}
+
+function save_file($temp_array, $filename) {
+	$handle = fopen($filename, "w");
+	$save_list = implode ("\n", $temp_array);
+	fwrite($handle, $save_list);
+	fclose($handle);
+}
+
+$contents_array=openfile($filename);
 
 if(!empty($_POST) && $_POST ["todo_item"] !== ''){
-array_push($contents_array, $_POST["todo_item"]);
+	array_push($contents_array, $_POST["todo_item"]);
+	save_file($contents_array, $filename);
 }
-
+	
 if (isset($_GET['remove'])) {
 	$key = $_GET["remove"];
+	// echo $key;
 	unset($contents_array[$key]);
+	save_file($contents_array, $filename);
 	header("Location: todo-list.php");
+	exit(0);
 }
 
-//$list = ['Take out the trash', 'Water the dog', 'Walk the plants', 'Milk the chicken'];
-foreach ($contents_array as $key => $value) {
-echo "<li>" . $value . " <a href = \"?remove=$key\">Remove item</a></li>";
+if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+    // Set the destination directory for uploads
+    $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
+    // Grab the filename from the uploaded file by using basename
+    $temp_file = basename($_FILES['file1']['name']);
+    // Create the saved filename using the file's original name and our upload directory
+    $saved_filename = $upload_dir . $temp_file;
+    // Move the file from the temp location to our uploads directory
+    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+    $append_list = openfile($saved_filename);
+    $contents_array = array_merge ($contents_array, $append_list);
+    save_file($contents_array, $filename);
 }
-unset($contents_array[$key]);
+
+foreach ($contents_array as $key => $value) {
+	echo "<li>" . $value . " <a href = \"?remove=$key\">Remove item</a></li>";
+}
+
 ?>
-<!-- <ul>
-	<li>Take out the trash</li>
-	<li>Water the dog</li>
-	<li>Walk the plants</li>
-	<li>Milk the chicken</li> -->
-<!-- Todo array
- -->
+
 </ul>
-	<h3>Select item to add to todo list:</h3>
-<form method="POST" action="todo-list.php">
+	<h3>Input item to add to todo list</h3>
+
+<form method="POST" enctype="multipart/form-data" action="todo-list.php">
 <p>
      <label for="todo_item">New todo item
      <input id="todo_item" name="todo_item" type="text" autofocus="autofocus">
     </p>
+    <h3>Upload file</h3>
+   <p>
+        <label for="file1">File to upload: </label>
+        <input type="file" id="file1" name="file1">
+    </p>
     <p>
        <input type="submit">
     </p>
+    <p>
 </form>
-
-<?php
-$handle = fopen($filename, "w");
-$saved_contents = implode ("\n", $contents_array);
-fwrite($handle, $saved_contents);
-fclose($handle);
-
-?>
 </body>
 </html>

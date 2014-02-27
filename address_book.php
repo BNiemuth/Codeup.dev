f0b4de46c84893977f1d5da775bfcce64f8ab09f
Barry Niemuth <bn3737@yahoo.com>
2014-02-27 09:28:08 -0600

    Push after adding function

--- address_book.php
+++ address_book.php
@@ -1,18 +1,28 @@
 <?php
+echo "<p>GET:</p>";
+var_dump($_GET);
+
+echo "<p>POST:</p>";
+var_dump($_POST);
 
 $address_book = [
-    ['The White House', '1600 Pennsylvania Avenue NW', 'Washington', 'DC', '20500', '202-555-1212'],
+    ['The White House', '1600 Pennsylvania Ave NW', 'Washington', 'DC', '20500', '202-555-1212'],
     ['Marvel Comics', 'P.O. Box 1527', 'Long Island City', 'NY', '11101', '718-555-1212'],
     ['LucasArts', 'P.O. Box 29901', 'San Francisco', 'CA', '94129-0901', '415-555-1212'],
 ];
 
-$handle = fopen('address_book.csv', 'w');
+function save_csv($array, $location){
+	$handle = fopen($location, 'w');
 
-foreach ($address_book as $fields) {
-    fputcsv($handle, $fields);
+	foreach ($array as $fields) {
+	    fputcsv($handle, $fields);
+	}
+
+	fclose($handle);
 }
 
-fclose($handle);
+save_csv($address_book,'address_book.csv');
+
 ?>
 
 <!DOCTYPE html>
@@ -21,50 +31,56 @@ fclose($handle);
 	<title>Address Book</title>
 </head>
 <body>
-	<h2>Address book - New entry</h2>
-<ul>
-	<?php	
-	foreach ($contents_array as $key => $value) {
-	echo "<li>" . $value . " <a href = \"?remove=$key\">Remove item</a></li>";
-}
-?>
+	<h2>Address Book</h2>
 
-</ul>
+<table>
+	<? foreach ($address_book as $address) { ?>
+		<tr>
+			<? foreach ($address as $item) { ?>
+			<td>
+				<?= $item; ?>
+			</td>
+			<? } ?>
+		</tr>
+	<? } ?>
+</table>
+	<h2>New Customer entry</h2>
+
 	<!-- <h4>Input new customer data</h4> -->
-
-	<form method="POST" enctype="multipart/form-data" action="address_book.csv">
+	<form method="POST" enctype="multipart/form-data" action="address_book.php">
 <p>
      <label for="cutomer_name">Customer name
      <input id="customer_name" name="customer_name" type="text" autofocus="autofocus">
 
     </p>
-	<form method="POST" enctype="multipart/form-data" action="address_book.csv">
 <p>
-     <label for="cutomer_name">Customer address
-     <input id="customer_name" name="customer_name" type="text" >
+     <label for="cutomer_address">Customer address
+     <input id="customer_address" name="customer_address" type="text" >
 
     </p>
-    <form method="POST" enctype="multipart/form-data" action="address_book.csv">
 <p>
-     <label for="cutomer_name">Customer city
-     <input id="customer_name" name="customer_name" type="text" >
+     <label for="cutomer_city">Customer city
+     <input id="customer_city" name="customer_city" type="text" >
 
     </p>
-	<form method="POST" enctype="multipart/form-data" action="address_book.csv">
 <p>
-     <label for="cutomer_name">Customer state
-     <input id="customer_name" name="customer_name" type="text" >
+     <label for="cutomer_state">Customer state
+     <input id="customer_state" name="customer_state" type="text" >
 
     </p>
-    <form method="POST" enctype="multipart/form-data" action="address_book.csv">
 <p>
-     <label for="cutomer_name">Customer zip
-     <input id="customer_name" name="customer_name" type="text" >
+     <label for="cutomer_zip">Customer zip
+     <input id="customer_zip" name="customer_zip" type="text" >
 
     </p>
-    <form method="POST" enctype="multipart/form-data" action="address_book.csv">
 <p>
-     <label for="cutomer_name">Customer phone number
-     <input id="customer_name" name="customer_name" type="text" >
+     <label for="cutomer_phone">Customer phone number
+     <input id="customer_phone" name="customer_phone" type="text" >
 
     </p>
+    <p>
+       <input type="submit">
+    </p>
+</form>
+</body>
+</html>

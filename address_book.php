ea48fcac562c9be6e085c7628a38d6f5d84af3a9
Barry Niemuth <bn3737@yahoo.com>
2014-02-28 09:30:33 -0600

    Polished

--- address_book.php
+++ address_book.php
@@ -1,15 +1,27 @@
 <?php
-echo "<p>GET:</p>";
-var_dump($_GET);
+// echo "<p>GET:</p>";
+// var_dump($_GET);
 
-echo "<p>POST:</p>";
-var_dump($_POST);
+// echo "<p>POST:</p>";
+// var_dump($_POST);
 
-$address_book = [
-    ['The White House', '1600 Pennsylvania Ave NW', 'Washington', 'DC', '20500', '202-555-1212'],
-    ['Marvel Comics', 'P.O. Box 1527', 'Long Island City', 'NY', '11101', '718-555-1212'],
-    ['LucasArts', 'P.O. Box 29901', 'San Francisco', 'CA', '94129-0901', '415-555-1212'],
-];
+$location = 'address_book.csv';
+$error_message = "";
+
+function read_csv($location) {
+	$handle = fopen($location, 'r');
+	$filesize = filesize($location);
+	if ($filesize > 0) {
+		while(($data = fgetcsv($handle)) !==FALSE) {
+			$contents[] = $data;
+		}
+
+	} else {
+		$contents = [];
+	}
+	fclose($handle);
+	return $contents;
+}
 
 function save_csv($array, $location){
 	$handle = fopen($location, 'w');
@@ -21,7 +33,34 @@ function save_csv($array, $location){
 	fclose($handle);
 }
 
-save_csv($address_book,'address_book.csv');
+function add_address($address_book, &$error_message){
+	$temp = $_POST;
+	if ($temp['customer_name'] == '' || ['customer_address'] == '' || ['customer_city'] == '' ||
+	['customer_state'] == '' || ['customer_zip'] == '') {
+		$error_message = '** It looks like you left something blank.** :(';
+
+	} else {
+		$address_book[] = $temp;
+	}
+	return $address_book;
+}
+
+// check for empty fields
+// if (empty($_POST['name'])) {
+// 	echo "Name field is empty";
+// } else {
+// 	echo "Name field is not empty";
+// }
+
+$address_book = read_csv ($location);
+
+if (!empty($_POST)) {
+	$address_book=add_address($address_book, $error_message);
+}
+
+// Add new entry to address_book
+
+save_csv($address_book,$location);
 
 ?>
 
@@ -34,42 +73,47 @@ save_csv($address_book,'address_book.csv');
 	<h2>Address Book</h2>
 
 <table>
-	<? foreach ($address_book as $address) { ?>
+	<? foreach ($address_book as $key => $address) { ?>
 		<tr>
 			<? foreach ($address as $item) { ?>
 			<td>
 				<?= $item; ?>
 			</td>
 			<? } ?>
+			<td><a href="?remove=<?=$key?>">Remove item</a></td>
 		</tr>
 	<? } ?>
 </table>
+
 	<h2>New Customer entry</h2>
+	<h5>* Required field<h5>
+
+<?= "<p><h4>{$error_message}</h4></p>" ?>
 
 	<!-- <h4>Input new customer data</h4> -->
 	<form method="POST" enctype="multipart/form-data" action="address_book.php">
 <p>
-     <label for="cutomer_name">Customer name
+     <label for="cutomer_name">Customer name *
      <input id="customer_name" name="customer_name" type="text" autofocus="autofocus">
 
     </p>
 <p>
-     <label for="cutomer_address">Customer address
+     <label for="cutomer_address">Customer address *
      <input id="customer_address" name="customer_address" type="text" >
 
     </p>
 <p>
-     <label for="cutomer_city">Customer city
+     <label for="cutomer_city">Customer city *
      <input id="customer_city" name="customer_city" type="text" >
 
     </p>
 <p>
-     <label for="cutomer_state">Customer state
+     <label for="cutomer_state">Customer state *
      <input id="customer_state" name="customer_state" type="text" >
 
     </p>
 <p>
-     <label for="cutomer_zip">Customer zip
+     <label for="cutomer_zip">Customer zip *
      <input id="customer_zip" name="customer_zip" type="text" >
 
     </p>
